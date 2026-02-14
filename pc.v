1557eb0ec3fd5ab3c7064c69fc7473043c51a226
Nikhil <nikhil.atkuru@students.iiit.ac.in>
2026-02-14 12:47:34 +0530

    Program Counter done

---
+++ pc.v
@@ -0,0 +1,10 @@
+module pc(input clk,input reset,input [63:0] pc_in,output reg [63:0] pc_out);
+    always @(posedge clk) begin
+        if(reset == 1'b1) begin
+            pc_out <= 64'b0;
+        end
+        else begin
+            pc_out <= pc_in;
+        end
+    end
+endmodule

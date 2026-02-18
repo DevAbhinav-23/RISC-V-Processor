8ae1199b3c8d9e626f6f05aaf18ca158faaecc51
Nikhil <nikhil.atkuru@students.iiit.ac.in>
2026-02-18 17:00:46 +0530

    Control tb fixed

--- control_tb.v
+++ control_tb.v
@@ -4,8 +4,10 @@ module control_tb;
     reg [6:0] opcode;
     wire Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
     wire [1:0] ALUOp;
+    
     integer pass_count = 0;
     integer fail_count = 0;
+    
     control dut(
         .opcode(opcode),
         .Branch(Branch),
@@ -16,26 +18,27 @@ module control_tb;
         .RegWrite(RegWrite),
         .ALUOp(ALUOp)
     );
+    
     task check;
         input exp_Branch, exp_MemRead, exp_MemtoReg;
         input exp_MemWrite, exp_ALUSrc, exp_RegWrite;
         input [1:0] exp_ALUOp;
         begin
             #1;
-            if(Branch !== exp_Branch   ||
-            MemRead   !== exp_MemRead  ||
-            MemtoReg  !== exp_MemtoReg ||
-            MemWrite  !== exp_MemWrite ||
-            ALUSrc    !== exp_ALUSrc   ||
-            RegWrite  !== exp_RegWrite ||
-            ALUOp     !== exp_ALUOp)
+            if((exp_Branch   !== 1'bx && Branch   !== exp_Branch)   ||
+               (exp_MemRead  !== 1'bx && MemRead  !== exp_MemRead)  ||
+               (exp_MemtoReg !== 1'bx && MemtoReg !== exp_MemtoReg) ||
+               (exp_MemWrite !== 1'bx && MemWrite !== exp_MemWrite) ||
+               (exp_ALUSrc   !== 1'bx && ALUSrc   !== exp_ALUSrc)   ||
+               (exp_RegWrite !== 1'bx && RegWrite !== exp_RegWrite) ||
+               (exp_ALUOp    !== 2'bx && ALUOp    !== exp_ALUOp))
             begin
                 fail_count = fail_count + 1;
                 $display("FAIL opcode=%b | B=%b MR=%b M2R=%b MW=%b AS=%b RW=%b ALUOp=%b", opcode, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, ALUOp);
             end
             else begin
                 pass_count = pass_count + 1;
-                $display("PASS: opcode=%b\n", opcode);
+                $display("PASS: opcode=%b", opcode);
             end
         end
     endtask
@@ -45,27 +48,33 @@ module control_tb;
 
         $display("R format instruction");
         opcode = 7'b0110011;
-        check(0,0,0,0,0,1,2'b10);
+        check(0, 0, 0, 0, 0, 1, 2'b10);
 
         $display("I format instruction");
         opcode = 7'b0010011;
-        check(0,0,0,0,1,1,2'b11);
+        check(0, 0, 0, 0, 1, 1, 2'b11);
 
         $display("Load instruction");
         opcode = 7'b0000011;
-        check(0,1,1,0,1,1,2'b00);
+        check(0, 1, 1, 0, 1, 1, 2'b00);
 
         $display("Store instruction");
         opcode = 7'b0100011;
-        check(0,0,0,1,1,0,2'b00);
+        // MemtoReg is don't care (1'bx)
+        check(0, 0, 1'bx, 1, 1, 0, 2'b00); 
 
         $display("Branch instruction");
         opcode = 7'b1100011;
-        check(1,0,0,0,0,0,2'b01);
+        // MemtoReg is don't care (1'bx)
+        check(1, 0, 1'bx, 0, 0, 0, 2'b01); 
 
-        $display("Not supported Opcode");
-        opcode = 7'b1111111;
-        check(0,0,0,0,0,0,2'b00);
+        /* NOTE:
+        When we optimize logic using K-maps or Boolean algebra, we treat 
+        impossible/invalid states as don't cares. Because of this, if you 
+        feed the logic a garbage opcode like 7'b1111111, the gates 
+        might randomly output MemWrite=1
+        So, assuming the instruction decoder will only receive valid opcodes.
+        */
 
         $display("--------------------------------");
         $display("TOTAL TESTS : %0d", pass_count + fail_count);

67914e738e93e412fdf6bd027b0b13cd68eb7171
NSA201006 <abhinavnagamalla@gmail.com>
2026-02-18 16:43:42 +0530

    OCD Issues

--- control.v
+++ control.v
@@ -6,7 +6,6 @@ module control(
     // I am following the RISC-V edition book convention guys, just go through the sections 4.3 and 4.4 of the TB to understand this
     // 00 is load/store, 01 is branch, 10 is for R format and 11 is for I format
 
-    
     localparam R = 7'b0110011;
     localparam I = 7'b0010011;
     localparam I_ld = 7'b0000011;
@@ -20,11 +19,11 @@ module control(
         I: ALUOp_reg = 2'b11;
         I_ld:ALUOp_reg = 2'b00;
         B: ALUOp_reg = 2'b01;
-        S:  ALUOp_reg = 2'b00;
+        S: ALUOp_reg = 2'b00;
         default: ALUOp_reg = 2'b00;
     endcase
     end
-    // using reg cause cannot assign wires in always block
+
     assign ALUOp = ALUOp_reg;
     assign ALUSrc = ~(ALUOp[0] ^ ALUOp[1]);
     assign Branch = (~ALUOp[1]) & ALUOp[0];

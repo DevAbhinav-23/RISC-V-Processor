b7fc4f122eae053dc4a880b963e98660504fc02a
NSA201006 <abhinavnagamalla@gmail.com>
2026-03-06 17:06:21 +0530

    Fixed IF_ID reg

--- Pipeline/IF_ID.v
+++ Pipeline/IF_ID.v
@@ -15,13 +15,13 @@ module IF_ID(
             PC_out <= 64'b0;
             inst_out <= 32'b0;
         end
-        else if(!stall) begin
-            PC_out <= PC_in;
-            inst_out <= inst_in;
-        end
         else if(flush) begin
             PC_out <= 64'b0;
             inst_out <= 32'b0;
+        end
+        else if(!stall) begin
+            PC_out <= PC_in;
+            inst_out <= inst_in;
         end
     end
 endmodule

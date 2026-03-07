72348d3c2d2e3a66eb7e06ed4e6e6a7d507357ce
Chronos <abhinavkota23@email.com>
2026-03-07 17:59:34 +0530

    Added the hazard and ID/EX pipeline

---
+++ Pipeline/ID_EX.v
@@ -0,0 +1,65 @@
+`timescale 1ns/1ps
+
+module ID_EX(
+    input reset,
+    input clk,
+
+    input [63:0] read_data1_in,
+    input [63:0] read_data2_in,
+    input [63:0] imm_in,
+    input [3:0] funct_in,
+    input [4:0] rd_in,
+    input [4:0] rs2_in,
+
+    input [1:0] ALUOp_in,
+    input ALUSrc_in,
+    input MemRead_in,
+    input MemtoReg_in,
+    input MemWrite_in,
+    input RegWrite_in,
+
+    output reg [63:0] read_data1_out,
+    output reg [63:0] read_data2_out,
+    output reg [63:0] imm_out,
+    output reg [3:0] funct_out,
+    output reg [4:0] rd_out,
+    output reg [4:0] rs2_out,
+
+    output reg [1:0] ALUOp_out,
+    output reg ALUSrc_out,
+    output reg MemRead_out,
+    output reg MemtoReg_out,
+    output reg MemWrite_out,
+    output reg RegWrite_out
+);
+    always @(posedge clk) begin
+        if (reset) begin
+            read_data1_out    <= 64'b0;
+            read_data2_out    <= 64'b0;
+            imm_out           <= 64'b0;
+            funct_out          <= 4'b0;
+            rd_out            <= 5'b0;
+            ALUOp_out         <= 2'b0;
+            ALUSrc_out        <= 1'b0;
+            MemRead_out       <= 1'b0;
+            MemtoReg_out      <= 1'b0;
+            MemWrite_out      <= 1'b0;
+            RegWrite_out      <= 1'b0;
+            rd2_out           <= 5'b0;
+        end
+        else begin
+            read_data1_out    <= read_data1_in;
+            read_data2_out    <= read_data2_in;
+            imm_out           <= imm_in;
+            funct_out          <= funct_in;
+            rd_out            <= rd_in;
+            ALUOp_out         <= ALUOp_in;
+            ALUSrc_out        <= ALUSrc_in;
+            MemRead_out       <= MemRead_in;
+            MemtoReg_out      <= MemtoReg_in;
+            MemWrite_out      <= MemWrite_in;
+            RegWrite_out      <= RegWrite_in;
+            rs2_out           <= rs2_in;
+        end
+    end
+endmodule
---
+++ Pipeline/hazard_detection.v
@@ -0,0 +1,21 @@
+// stall = 1 means stall the pipeline (PC and IF/ID hold values)
+// flush = 1 means flush the ID/EX control signals to NOP
+
+module hazard_detection(
+    input [4:0] rs1_IFID,
+    input [4:0] rs2_IFID,
+    input [4:0] rd_IDEX,
+    input MemRead_IDEX,
+    input [4:0] rd_EXMEM,
+    input MemRead_EXMEM,
+    output reg stall
+);
+    always @(*) begin
+        if(MemRead_IDEX && (rd_IDEX != 5'b0) && ((rd_IDEX == rs1_IFID) || (rd_IDEX == rs2_IFID)))
+            stall = 1'b1;
+        else if(MemRead_EXMEM && (rd_EXMEM != 5'b0) && ((rd_EXMEM == rs1_IFID) || (rd_EXMEM == rs2_IFID)))
+            stall = 1'b1;
+        else
+            stall = 1'b0;
+    end
+endmodule
